336cccf1918efaced2b570da935a0816fedc2d96
Henrik Johansson <johanssonhenrik91@gmail.com>
2014-03-17 15:32:53 +0100

    Update Controller.h
    Final

--- Controller/Controller.h
+++ Controller/Controller.h
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Controller.h
  *
  * Created: 2014-03-07 16:13:46
@@ -16,19 +16,16 @@ typedef struct{
 	Object super;
 	int northqueue;				//Keep track of the number of cars in each queue.
 	int southqueue;
-	int timer;					//Used to send USART interrupt periodically.
-	int maxiterationNorth;
-	int maxiterationSouth;
 	AVRGUI *gui;
 	uint8_t Data;
-	//USART receivedUSART;
+	int previousqueue;
 }Controller;
 
-void northTL(Controller *this, int carDecInc);		//TrafficLight methods. Used for switching between ON/OFF State.
+void trafficlights(Controller *this);		//TrafficLight methods. Used for switching between ON/OFF State.
 void southTL(Controller *this, int carDecInc);
 void bitwiseUSART(Controller *this, uint8_t Data);
 void sendtoPC(Controller *this, uint8_t Data);
 
-#define initController(northqueue,southqueue,timer,maxiterationNorth,maxiterationSouth,gui,Data){initObject(),northqueue,southqueue,timer,maxiterationNorth,maxiterationSouth,gui,Data};
+#define initController(northqueue,southqueue,gui,Data, previousqueue){initObject(),northqueue,southqueue,gui,Data, previousqueue};
 
 #endif __CONTROLLER_H_
